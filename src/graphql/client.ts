73fc2f9f2904b17b0d07edad6fef6ae68e507fb3
masaki-koide <ikasama0324@gmail.com>
2020-06-24 22:19:12 +0900

    chore: eslint fix

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
+
 import { getSdk } from '../generated/graphql'
 
 if (!process.env.CMS_API_URL) {
+  // eslint-disable-next-line no-console -- Due to build time error
   console.error('🥺The environment variable CMS_API_URL is not set.')
   process.exit(1)
 }
