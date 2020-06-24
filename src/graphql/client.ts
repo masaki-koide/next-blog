68cf0769b2f2084e876630cc616b28e50f7046f6
masaki-koide <ikasama0324@gmail.com>
2020-06-24 23:50:54 +0900

    fix: set Authorization in request header

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,11 +2,18 @@ import { GraphQLClient } from 'graphql-request'
 
 import { getSdk } from '../generated/graphql'
 
-if (!process.env.CMS_API_URL) {
+if (!process.env.CMS_API_URL || !process.env.CMS_API_TOKEN) {
   // eslint-disable-next-line no-console -- Due to build time error
-  console.error('🥺The environment variable CMS_API_URL is not set.')
+  console.error(
+    '🥺The environment variable CMS_API_URL and CMS_API_TOKEN is not set.'
+  )
   process.exit(1)
 }
 
-const client = new GraphQLClient(process.env.CMS_API_URL)
+const client = new GraphQLClient(process.env.CMS_API_URL, {
+  headers: {
+    Authorization: `Bearer ${process.env.CMS_API_TOKEN}`,
+  },
+})
+
 export const sdk = getSdk(client)
