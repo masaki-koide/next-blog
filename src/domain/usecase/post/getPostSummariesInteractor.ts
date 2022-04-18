31c0a8aaca2476aa743d04e75a094275fff9eb5c
masaki-koide <ikasama0324@gmail.com>
2022-04-18 10:51:31 +0900

    feat: get posts from qiita

--- src/domain/usecase/post/getPostSummariesInteractor.ts
+++ src/domain/usecase/post/getPostSummariesInteractor.ts
@@ -1,4 +1,4 @@
-import { injectable, inject } from 'tsyringe'
+import { injectable, inject, injectAll } from 'tsyringe'
 
 import { injectionTokens } from '../../../di/token'
 
@@ -9,11 +9,20 @@ import { PostRepository } from './interface/repository'
 export class GetPostSummariesInteractor implements GetPostSummariesUsecase {
   constructor(
     @inject(injectionTokens.postRepository)
-    private postRepository: PostRepository
+    private postRepository: PostRepository,
+    @injectAll(injectionTokens.externalPostRepositories)
+    private externalPostRepositories: PostRepository[]
   ) {}
 
   async handle() {
-    const posts = await this.postRepository.getSummaries()
-    return posts.map(post => post.toObject())
+    const posts = await Promise.all([
+      this.postRepository.getSummaries(),
+      ...this.externalPostRepositories.map(repo => repo.getSummaries()),
+    ])
+
+    return posts
+      .flat()
+      .map(post => post.toObject())
+      .sort((a, b) => (a.date > b.date ? -1 : 1))
   }
 }
