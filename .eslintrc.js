3df7f4fc86364d09349896691f3fe153ff2dd20e
masaki-koide <ikasama0324@gmail.com>
2022-04-18 10:47:13 +0900

    chore: disable react/require-defalut-props rule of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   extends: ['thai-soup'],
   ignorePatterns: ['.eslintrc.js', 'babel.config.js'],
   parserOptions: {
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
-    'no-useless-constructor': 'off'
+    'no-useless-constructor': 'off',
   },
 }
