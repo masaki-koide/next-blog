c01d4fd1badf5c99d2000dfd87c37ed39d5064b4
masaki-koide <ikasama0324@gmail.com>
2022-05-04 23:22:19 +0900

    chore: fix site title

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
 
+import { TITLE } from '../constants'
+
 export function Layout({ children }: PropsWithChildren<unknown>) {
   return (
     <>
       <header className="h-16 border-b border-slate-300 bg-white">
         <div className="h-full max-w-screen-laptop mx-auto px-8 py-4 text-2xl font-bold tracking-widest drop-shadow">
-          <Link href="/">MARKEY.DEV</Link>
+          <Link href="/">{TITLE}</Link>
         </div>
       </header>
       <main className="max-w-screen-laptop mx-auto px-8 py-8">{children}</main>
