1d9c59cb37ec5c6dfc8b12adefb4010a312ce621
masaki-koide <ikasama0324@gmail.com>
2022-05-05 02:17:44 +0900

    feat: add external link icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { getPostSummariesInteractor } from '../di/container'
 import { PostSummaryDto } from '../domain/entity/postSummary'
 import { Layout } from '../components/Layout'
 import { Meta } from '../components/Meta'
+import { ExternalLink } from '../components/icons'
 
 type Props = {
   posts: PostSummaryDto[]
@@ -26,12 +27,21 @@ const Home: NextPage<Props> = ({ posts }) => {
                 <a
                   className="block px-6 py-12"
                   href={post.url ?? `/posts/${post.slug}`}
-                  rel={post.externalSite ? 'norefferer noopener' : ''}
+                  rel={post.externalSite ? 'noopener noreferrer' : ''}
                   target={post.externalSite ? '_blank' : '_self'}
                   title={post.title}
                 >
-                  <div className="flex justify-between">
-                    <div className="text-xl font-bold text-ellipsis whitespace-nowrap overflow-hidden">
+                  <div className="flex justify-between relative">
+                    {post.externalSite && (
+                      <div className="absolute mt-0.5">
+                        <ExternalLink height={24} width={24} />
+                      </div>
+                    )}
+                    <div
+                      className={`text-xl font-bold text-ellipsis whitespace-nowrap overflow-hidden ${
+                        post.externalSite ? 'pl-8' : ''
+                      }`}
+                    >
                       {post.title}
                     </div>
                     <div className="text-slate-400 text-sm">{post.date}</div>
